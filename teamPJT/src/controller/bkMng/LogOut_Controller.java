d3991a99da1df937f6dda3f5dfc0dae19fb6f804
ORC <ORC@DESKTOP-69VHU5A>
2024-06-17 12:54:50 +0900

    ...

---
+++ teamPJT/src/controller/bkMng/LogOut_Controller.java
@@ -0,0 +1,21 @@
+package controller.bkMng;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import controller.Controller;
+
+public class LogOut_Controller implements Controller{
+
+	@Override
+	public String execute(Map<String, Object> model) throws Exception {
+		
+		HttpSession session = (HttpSession)model.get("session");
+		session.invalidate();
+		
+		return "login.do";
+		
+	}
+
+}
