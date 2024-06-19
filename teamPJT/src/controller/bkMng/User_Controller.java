05e88ecb1dc7446b9083aeb93427c08444d1a8fc
ORC <ORC@DESKTOP-69VHU5A>
2024-06-19 17:01:58 +0900

    ...

--- teamPJT/src/controller/bkMng/User_Controller.java
+++ teamPJT/src/controller/bkMng/User_Controller.java
@@ -19,20 +19,58 @@ public class User_Controller implements Controller{
 	@Override
 	public String execute(String flag, Map<String, Object> model) throws Exception {
 		
-		
-		//UserVO user = (UserVO) userDAO.selectList();	
 		String retrunValue = "";
-		
+	
 		if (flag.equals("list")) {
 		
-			UserVO user = (UserVO)model.get("user");
-	
-			model.put("userlist", userDAO.selectList(user));
+			retrunValue = list(flag, model);
 			
-			retrunValue = "bk_user.jsp";
+		}else if (flag.equals("getOne")) {
 			
+			retrunValue = getOne(flag, model);
+			
+		}else if (flag.equals("modify")) {
+			
+			retrunValue = modify(flag, model);
+			
+		}else if (flag.equals("del")) {
+			
+			retrunValue = del(flag, model);
 		}
+		
 		return retrunValue;
+		
 	}
+	
+	
+	
+	public String list(String flag, Map<String, Object> model) throws Exception {
+		
+		UserVO user = (UserVO)model.get("user");	
+		model.put("userlist", userDAO.selectList(user));
+		
+		return "bk_user.jsp";
+		
+	}
+	
+	public String getOne(String flag, Map<String, Object> model) throws Exception {
+		
+		return null;
+		
+	}
+	
+	
+	public String modify(String flag, Map<String, Object> model) throws Exception {
+		
+		return null;
+		
+	}	
+	
+	public String del(String flag, Map<String, Object> model) throws Exception {
+		
+		return null;
+		
+	}		
 
+	
 }
