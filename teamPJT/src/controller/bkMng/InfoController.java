e4ee141c571e63c9451dd31407b16cc97aba5bf6
ORC <ORC@DESKTOP-69VHU5A>
2024-07-01 10:04:33 +0900

    ...

--- teamPJT/src/controller/bkMng/InfoController.java
+++ teamPJT/src/controller/bkMng/InfoController.java
@@ -48,8 +48,8 @@ public class InfoController implements Controller{
 			
 			List<CategoryVO> cvo  = cateDAO.bkCateList();
 			
-			//if(vo != null && vo.getUser_role().equals("A")) { 
-			if(vo != null) {
+			if(vo != null && vo.getUser_role().equals("A")) { 
+			//if(vo != null) {
 				
 				System.out.println(" id : " + vo.getUser_id());
 				System.out.println(" name : " + vo.getUser_name());
