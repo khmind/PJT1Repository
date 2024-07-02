2539cef2ab07731211b10a1252e9949fc0451d9a
ORC <ORC@DESKTOP-69VHU5A>
2024-07-02 12:31:42 +0900

    ...

--- teamPJT/src/controller/frMng/MyPageController.java
+++ teamPJT/src/controller/frMng/MyPageController.java
@@ -4,26 +4,18 @@ import java.util.Map;
 
 import controller.Controller;
 import dao.bkMng.CategoryDAO;
-import dao.frMng.RecipeFrDAO;
-import vo.bkMng.RecipeVO;
 
 public class MyPageController implements Controller{
 	
-	
-	CategoryDAO cateDAO;
-	RecipeFrDAO recipeFrDAO;
+	CategoryDAO cateDAO;	
 	
 	public MyPageController setDAO(CategoryDAO cateDAO) {		
-		this.cateDAO = cateDAO;
-		this.recipeFrDAO = recipeFrDAO;
+		this.cateDAO = cateDAO;		
 		return this;
 	}	
 
 	@Override
 	public String execute(String flag, Map<String, Object> model) throws Exception {
-		// TODO Auto-generated method stub
-		//return null;
-		
 		
 		model.put("navbar", cateDAO.frCateList());
 		return "frMng/fr_user_info.jsp";
